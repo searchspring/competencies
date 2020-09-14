ea612c2af186cb3b935858c9bc95921c36065477
wafer-bw <ben.jz.wakeford@gmail.com>
2020-09-14 13:17:41 -0400

    supress logging in tests

--- main_test.go
+++ main_test.go
@@ -1,17 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestMain(m *testing.M) {
+	log.SetOutput(ioutil.Discard)
+	os.Exit(m.Run())
+}
 
 func TestProcessInherited(t *testing.T) {
 	result, err := processInherits("#original \n##Skills\n<inherit doc=\"../test1.md\"/>\n   a  ", false)
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(result)
+	log.Println(result)
 	require.Equal(t, "#original \n##Skills\n<skills>\nbreakdancing\n</skills>\n<skills>\nfigure skating\n</skills>\n<skills>\nkung fu\n</skills>\n   a", result)
 }
