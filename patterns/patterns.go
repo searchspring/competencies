fdb7c15f0fe7bd138ae67015de662885be0b033f
wafer-bw <ben.jz.wakeford@gmail.com>
2022-07-08 16:44:19 -0400

    Add TODO to pattern

--- patterns/patterns.go
+++ patterns/patterns.go
@@ -4,7 +4,7 @@ import "regexp"
 
 var (
 	RoleTitle             *regexp.Regexp = regexp.MustCompile(`^(?:# ){1}(.*)`)
-	Skills                *regexp.Regexp = regexp.MustCompile(`(?m)^(?P<amount>\d+)?(?: of )?(?P<name>[^:\r\n]+)+:?(?P<level>\d+)?$`)
+	Skills                *regexp.Regexp = regexp.MustCompile(`(?m)^(?P<amount>\d+)?(?: of )?(?P<name>[^:\r\n]+)+:?(?P<level>\d+)?$`) // TODO: this doesn't capture skill "1password" properly
 	InheritNode           *regexp.Regexp = regexp.MustCompile(`<inherit doc="([^"]+)"\/>`)
 	SkillsContainer       *regexp.Regexp = regexp.MustCompile(`<skills>[\r\n]([^<]+)<\/skills>`)
 	CompetencyHeaderSplit *regexp.Regexp = regexp.MustCompile(`(?m)^# `)
